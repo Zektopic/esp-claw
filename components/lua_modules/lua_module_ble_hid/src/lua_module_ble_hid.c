04fe596d023215658aeea84d0501a01a53187953
loop <lishenhang@espressif.com>
2026-05-20 16:59:44 +0800

    feat(edge_agent): enable Lua modules and tune config

--- components/lua_modules/lua_module_ble_hid/src/lua_module_ble_hid.c
+++ components/lua_modules/lua_module_ble_hid/src/lua_module_ble_hid.c
@@ -27,7 +27,6 @@
 #include "lua.h"
 #include "nimble/nimble_port.h"
 #include "nimble/nimble_port_freertos.h"
-#include "nvs_flash.h"
 #include "sdkconfig.h"
 #include "services/gap/ble_svc_gap.h"
 
@@ -361,20 +360,6 @@ static int ble_hid_gap_event(struct ble_gap_event *event, void *arg)
     return 0;
 }
 
-static esp_err_t ble_hid_nvs_init(void)
-{
-    esp_err_t err = nvs_flash_init();
-
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        err = nvs_flash_init();
-    }
-    if (err == ESP_ERR_INVALID_STATE) {
-        err = ESP_OK;
-    }
-    return err;
-}
-
 static esp_err_t ble_hid_controller_start(void)
 {
     esp_err_t err;
@@ -433,11 +418,6 @@ static esp_err_t ble_hid_stack_prepare(void)
 
     if (s_nimble_inited) {
         return ESP_OK;
-    }
-
-    err = ble_hid_nvs_init();
-    if (err != ESP_OK) {
-        return err;
     }
 
     if (s_host_sync_sem == NULL) {
